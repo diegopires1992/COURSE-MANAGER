27431c7602630c5778d08b9be97523d990f65254
Diego Pires <diego__pires@hotmail.com>
2022-06-07 22:43:43 -0300

    merge

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+})
+
+
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: "Angular: Forms",
+        imageUrl: "",
+        price: 300,
+        duration: 125,
+        rating: 5.4,
+        code: 'AXPSADSAD',
+        releaseDate: '2021-06-07'
+      }, {
+        id: 2,
+        name: "Angular: HTTP",
+        imageUrl: "",
+        price: 500,
+        duration: 80,
+        rating: 4,
+        code: 'AXPSADSAD',
+        releaseDate: '2021-06-07'
+      }
+    ]
+  }
+
+}
